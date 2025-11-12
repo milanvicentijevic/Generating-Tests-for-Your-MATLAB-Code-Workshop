0b97fe0b8949578f435fc4963bf8cf60a29c3539
Milan <milan.vicentijevic@inetec.hr>
2025-11-12 12:44:03 +0000

    Added tests and fixed isLeapYear code

--- code/isLeapYear.m
+++ code/isLeapYear.m
@@ -19,12 +19,9 @@ isDivBy100 = mod(year,100) == 0;
 isDivBy4   = mod(year,  4) == 0;
 
 % Check for leap year
-if isDivBy400
+% Check for leap year
+if isDivBy400 || (isDivBy4 && ~isDivBy100)
     isLeap = true;
-elseif isDivBy4
-    isLeap = true;
-elseif isDivBy100
-    isLeap = false;
 else
     isLeap = false;
 end
